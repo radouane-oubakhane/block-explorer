b48257947d8c918b198e5af9ce4440c62de00141
Radouane OUBAKHANE <radouane.oubakhane@usms.ac.ma>
2023-04-27 15:32:07 +0100

    Implement dark mode and build the color mode button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import {HStack, Text} from "@chakra-ui/react";
+import ColorModeSwitch from "./ColorModeSwitch.jsx";
 
 
 const Header = () => {
@@ -6,7 +7,7 @@ const Header = () => {
     return (
         <HStack justifyContent="space-between" padding={2}>
             <Text>ETH Price</Text>
-            ColorModeSwitch
+            <ColorModeSwitch />
         </HStack>
     )
 
