ddc674a55a1156391700427841c3257ef092425e
Radouane OUBAKHANE <radouane.oubakhane@usms.ac.ma>
2023-04-27 15:15:25 +0100

    Build a responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Grid, GridItem} from "@chakra-ui/react";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Grid templateAreas={`"header" "nav" "main" "footer"`}
+            templateColumns="1fr"
+      >
+
+          <GridItem area="header">
+              Header
+          </GridItem>
+
+          <GridItem area="nav">
+              NavBar
+          </GridItem>
+
+          <GridItem area="main">
+              main
+          </GridItem>
+
+          <GridItem area="footer">
+              Footer
+          </GridItem>
+      </Grid>
   )
 }
 
